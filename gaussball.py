53554d37134947f023b5e431341c6ae3c3bb1a39
fcseidl <fcseidl@umich.edu>
2021-06-23 15:18:20 -0400

    aspect ratio bug fixed

--- gaussball.py
+++ gaussball.py
@@ -22,54 +22,52 @@ Cov = np.array([
 n_balls = 2
 
 
-#### specify colors ####
+#### visualization parameters ####
 
+asp = (800, 900)        # h, w
 color_comps = np.array([1., 0., 0.])
 
 
-# simply plot density with principal components
+#### get principal components and use covariance scale to define pixel space transform ####
 
-asp = (800, 800)
-
-# get principal components
 vals, vecs = linalg.eig(Cov)
 pcmps = vecs * np.sqrt(vals)
-scale = np.sqrt(vals.max()) * 4 / np.mean(asp)
+pixels_per_unit = np.mean(asp) / (np.sqrt(vals.max()) * 4)
 
-def pixel2plane(u, v):
-    x = u - asp[0] / 2
-    y = asp[1] / 2 - v
-    x *= scale
-    y *= scale
+def pixel2plane(u, v) -> tuple:
+    x = u - asp[1] / 2
+    y = asp[0] / 2 - v
+    x /= pixels_per_unit
+    y /= pixels_per_unit
     return x, y
 
-def plane2pixel(x, y):
-    u = x / scale
-    v = -y / scale
-    u += asp[0] / 2
-    v += asp[1] / 2
+def plane2pixel(x, y) -> tuple:
+    u = x * pixels_per_unit
+    v = -y * pixels_per_unit
+    u += asp[1] / 2
+    v += asp[0] / 2
     return int(u), int(v)
 
-# background is gaussian blur
+
+#### get gaussian blur with principal components for background ####
+
 idx = np.indices(asp)
-x, y = pixel2plane(idx[0], idx[1])
+x, y = pixel2plane(idx[1], idx[0])
 pts = np.dstack((x, y))
 density = multivariate_normal.pdf(pts, cov=Cov)
-greyscale = (1 - density / np.max(density))
+greyscale = 1 - density / np.max(density)
 # this is a hack to get an rgb image
 background = np.empty((asp[0], asp[1], 3))
 for c in range(3):
     background[:, :, c] = greyscale
 
-# overlay principal components
 origin = plane2pixel(0, 0)
 cmp0 = plane2pixel(pcmps[0, 0], pcmps[0, 1])
 cmp1 = plane2pixel(pcmps[1, 0], pcmps[1, 1])
+cv2.line(background, origin, cmp0, color_comps)
+cv2.line(background, origin, cmp1, color_comps)
 
-img = background.copy()
-cv2.line(img, origin, cmp0, color_comps)
-cv2.line(img, origin, cmp1, color_comps)
 
-cv2.imshow('image', img)
+cv2.imshow('image', background)
 cv2.waitKey()
 

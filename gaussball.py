3df75036bb21e0a839888e18d761f9516b2ed23d
fcseidl <fcseidl@umich.edu>
2021-06-23 15:50:20 -0400

    reworked image coords again because opencv is terrible

--- gaussball.py
+++ gaussball.py
@@ -17,14 +17,15 @@ import cv2
 dim = 2                     # TODO: what about different dimensions?
 Cov = np.array([
     [1, 0],
-    [0, 1]
+    [0, 5]
 ])          
 n_balls = 2
 
 
 #### visualization parameters ####
 
-asp = (800, 900)        # h, w
+view_w = 640
+view_h = 480
 color_comps = np.array([1., 0., 0.])
 
 
@@ -32,32 +33,32 @@ color_comps = np.array([1., 0., 0.])
 
 vals, vecs = linalg.eig(Cov)
 pcmps = vecs * np.sqrt(vals)
-pixels_per_unit = np.mean(asp) / (np.sqrt(vals.max()) * 4)
+pixels_per_unit = np.mean([view_w, view_h]) / (np.sqrt(vals.max()) * 4)
 
 def pixel2plane(u, v) -> tuple:
-    x = u - asp[1] / 2
-    y = asp[0] / 2 - v
+    x = u - view_w / 2
+    y = view_h / 2 - v
     x /= pixels_per_unit
     y /= pixels_per_unit
     return x, y
 
 def plane2pixel(x, y) -> tuple:
-    u = x * pixels_per_unit
-    v = -y * pixels_per_unit
-    u += asp[1] / 2
-    v += asp[0] / 2
+    v = x * pixels_per_unit
+    u = -y * pixels_per_unit
+    v += view_w / 2
+    u += view_h / 2
     return int(u), int(v)
 
 
 #### get gaussian blur with principal components for background ####
 
-idx = np.indices(asp)
-x, y = pixel2plane(idx[1], idx[0])
+idx = np.indices((view_w, view_h))
+x, y = pixel2plane(idx[0], idx[1])
 pts = np.dstack((x, y))
 density = multivariate_normal.pdf(pts, cov=Cov)
 greyscale = 1 - density / np.max(density)
 # this is a hack to get an rgb image
-background = np.empty((asp[0], asp[1], 3))
+background = np.empty((view_w, view_h, 3))
 for c in range(3):
     background[:, :, c] = greyscale
 
@@ -68,6 +69,6 @@ cv2.line(background, origin, cmp0, color_comps)
 cv2.line(background, origin, cmp1, color_comps)
 
 
-cv2.imshow('image', background)
+cv2.imshow('image', background)#cv2.transpose(background))
 cv2.waitKey()
 

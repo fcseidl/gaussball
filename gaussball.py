8bc278af7693a44b59e04aba6e88cf05202dbe56
fcseidl <fcseidl@umich.edu>
2021-06-23 22:59:43 -0400

    support higher dimensions

--- gaussball.py
+++ gaussball.py
@@ -15,12 +15,9 @@ import cv2
 
 #### specify problem instance ####
 
-dim = 2                     # TODO: what about different dimensions?
-Cov = np.array([
-    [6, 0],
-    [0.2, 0.8]
-])          
-n_balls = 3
+dim = 2
+Cov = np.identity(dim)      
+n_balls = 5
 
 # empirically estimate uncovered mass
 def cost(soln, n_samp=1000) -> float:
@@ -41,52 +38,55 @@ color_best = np.array([0., 1., 0.])     # green
 color_current = np.array([0., 0., 1.])  # red
 
 
-#### get principal components and use covariance scale to define pixel space transform ####
-# NOTE: we work with transposed images because opencv's image coordinate scheme is moronic
 
-vals, vecs = linalg.eig(Cov)
-pcmps = vecs * np.sqrt(vals)
-pixels_per_unit = int(np.mean([view_w, view_h]) / (np.sqrt(vals.max()) * 4))
+if dim == 2:
 
-def pixel2plane(u, v) -> tuple:
-    x = u - view_w / 2
-    y = view_h / 2 - v
-    x /= pixels_per_unit
-    y /= pixels_per_unit
-    return x, y
+    #### get principal components and use covariance scale to define pixel space transform ####
+    # NOTE: we work with transposed images because opencv's image coordinate scheme is moronic
 
-def plane2pixel(x, y) -> tuple:
-    v = x * pixels_per_unit
-    u = -y * pixels_per_unit
-    v += view_w / 2
-    u += view_h / 2
-    return int(u), int(v)
+    vals, vecs = linalg.eig(Cov)
+    pcmps = vecs * np.sqrt(vals)
+    pixels_per_unit = int(np.mean([view_w, view_h]) / (np.sqrt(vals.max()) * 4))
+
+    def pixel2plane(u, v) -> tuple:
+        x = u - view_w / 2
+        y = view_h / 2 - v
+        x /= pixels_per_unit
+        y /= pixels_per_unit
+        return x, y
+
+    def plane2pixel(x, y) -> tuple:
+        v = x * pixels_per_unit
+        u = -y * pixels_per_unit
+        v += view_w / 2
+        u += view_h / 2
+        return int(u), int(v)
 
 
-#### get gaussian blur with principal components for background ####
+    #### get gaussian blur with principal components for background ####
 
-idx = np.indices((view_w, view_h))
-x, y = pixel2plane(idx[0], idx[1])
-pts = np.dstack((x, y))
-density = multivariate_normal.pdf(pts, cov=Cov)
-greyscale = 1 - density / np.max(density)
-# this is a hack to get an rgb image
-background = np.empty((view_w, view_h, 3))
-for c in range(3):
-    background[:, :, c] = greyscale
+    idx = np.indices((view_w, view_h))
+    x, y = pixel2plane(idx[0], idx[1])
+    pts = np.dstack((x, y))
+    density = multivariate_normal.pdf(pts, cov=Cov)
+    greyscale = 1 - density / np.max(density)
+    # this is a hack to get an rgb image
+    background = np.empty((view_w, view_h, 3))
+    for c in range(3):
+        background[:, :, c] = greyscale
 
-origin = plane2pixel(0, 0)
-cmp0 = plane2pixel(pcmps[0, 0], pcmps[0, 1])
-cmp1 = plane2pixel(pcmps[1, 0], pcmps[1, 1])
-cv2.line(background, origin, cmp0, color_comps)
-cv2.line(background, origin, cmp1, color_comps)
+    origin = plane2pixel(0, 0)
+    cmp0 = plane2pixel(pcmps[0, 0], pcmps[0, 1])
+    cmp1 = plane2pixel(pcmps[1, 0], pcmps[1, 1])
+    cv2.line(background, origin, cmp0, color_comps)
+    cv2.line(background, origin, cmp1, color_comps)
 
 
 #### wrap cost in functor which visualizes search ####
 
 class Solver(Annealer):
 
-    _show_every = 50
+    _show_every = 100
 
     # spherical gaussian nudges
     def _spherical_noise(self) -> np.ndarray:
@@ -134,7 +134,7 @@ class Solver(Annealer):
             #print('found solution with cost of', c)
             self._min = c
             self._argmin = self.state.copy()
-        if self._n_steps % self._show_every == 0:
+        if dim == 2 and self._n_steps % self._show_every == 0:
             img = background.copy()
             for center in self.state:
                 u, v = plane2pixel(center[0], center[1])
@@ -156,7 +156,7 @@ x0 = np.atleast_2d(x0)
 coefs = {
     #'spherical noise':  1e-2,
     'shaped noise':     1e-2,
-    'inward pull':      1e-4,
+    'inward pull':      5e-4,
     'ball repulsion':   1e-4
 }
 
@@ -165,11 +165,12 @@ centers, c = solver.anneal()
 print('\nBest solution:\n', centers)
 print('Cost:', c)
 
-cv2.destroyAllWindows()
-img = background.copy()
-for center in centers:
-    u, v = plane2pixel(center[0], center[1])
-    cv2.circle(img, (u,v), pixels_per_unit, color_best)
-cv2.imshow('Best solution found, with cost of %f' % c, cv2.transpose(img))
-cv2.waitKey()
-cv2.destroyAllWindows()
+if dim == 2:
+    cv2.destroyAllWindows()
+    img = background.copy()
+    for center in centers:
+        u, v = plane2pixel(center[0], center[1])
+        cv2.circle(img, (u,v), pixels_per_unit, color_best)
+    cv2.imshow('Best solution found, with cost of %f' % c, cv2.transpose(img))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
